219e39b89a82217d3f4ee42c94ab10a84da457ac
ntest8883-prog <ntest8883@gmail.com>
2026-09-25 22:39:16 +0200

    H1-A: expose restore state to NT layer

--- h0/simplevisor/shv_x.h
+++ h0/simplevisor/shv_x.h
@@ -132,3 +132,6 @@ extern UINT64 ShvH0TestPagePhysicalAddress;
 extern volatile long ShvH0EptTrapCount;
 extern volatile UINT64 ShvH0LastGuestPhysicalAddress;
 extern volatile UINT64 ShvH0LastExitQualification;
+extern UINT64 ShvH1TestPageVirtualAddress;
+extern UINT64 ShvH1BackingPageVirtualAddress;
+extern volatile long ShvH1RestoreCount;
